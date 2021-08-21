1b5ba187ffbb79b219514dd0df0a4826b073185b
Kap1tan1 <ilsafilsaf66@gmail.com>
2021-08-21 14:23:29 +0300

    first_commit

---
+++ src/main/java/itis/project/ilgamov/anisurfbackend/AnisurfBackendApplication.java
@@ -0,0 +1,13 @@
+package itis.project.ilgamov.anisurfbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnisurfBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnisurfBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/itis/project/ilgamov/anisurfbackend/AnisurfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package itis.project.ilgamov.anisurfbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnisurfBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
